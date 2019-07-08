384eaee6116d96f7c4835e44e5928efaa9895581
student <sorush.kh68@gmail.com>
2019-07-08 09:17:03 +0100

    The security file is refactored now

--- code/security.py
+++ code/security.py
@@ -1,26 +1,28 @@
 from user import User
 import sqlite3
 def authenticate(username , password):
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
-    result = cursor.execute("SELECT * FROM users WHERE username=? AND password=? ",(username, password,))
-    row = result.fetchone()
-    if row:
-        user = User(*row)
-    else:
-        user = None
-    connection.close()
-    return user
+    # connection = sqlite3.connect("data.db")
+    # cursor = connection.cursor()
+    # result = cursor.execute("SELECT * FROM users WHERE username=? AND password=? ",(username, password,))
+    # row = result.fetchone()
+    # if row:
+    #     user = User(*row)
+    # else:
+    #     user = None
+    # connection.close()
+    # return user
+    return User.find_by_username(username , password)
 
 def identify(payload):
-    user_id = payload["identity"]
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
-    result = cursor.execute("SELECT * FROM users WHERE id=? ",(user_id,))
-    row = result.fetchone()
-    if row:
-        user = User(*row)
-    else:
-        user = None
-    connection.close()
-    return user
+    # user_id = payload["identity"]
+    # connection = sqlite3.connect("data.db")
+    # cursor = connection.cursor()
+    # result = cursor.execute("SELECT * FROM users WHERE id=? ",(user_id,))
+    # row = result.fetchone()
+    # if row:
+    #     user = User(*row)
+    # else:
+    #     user = None
+    # connection.close()
+    # return user
+    return User.find_by_user_id(payload["identity"])
--- code/user.py
+++ code/user.py
@@ -1,5 +1,29 @@
+import sqlite3
 class User:
     def __init__(self, _id , name , username):
         self.id = _id
         self.name = name
         self.username = username
+    @classmethod
+    def find_by_username(cls ,username, password):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM users WHERE username=? AND password=?",(username,password))
+        row = result.fetchone()
+        connection.commit()
+        connection.close()
+        if row:
+            return cls(*row)
+        else:
+            return None
+    def find_by_user_id(cls, user_id):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM users WHERE id = user_id",(user_id,))
+        row = result.fetchone()
+        connection.commit()
+        connection.close()
+        if row:
+            return cls(*row)
+        else:
+            return None
