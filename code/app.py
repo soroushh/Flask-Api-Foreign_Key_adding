35640d0d886b8e9da2c90d6d8f6088d9e1a8b283
student <sorush.kh68@gmail.com>
2019-07-08 10:50:01 +0100

    An environment variable is used in our python application now.

--- code/app.py
+++ code/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_jwt import JWT, jwt_required
@@ -6,7 +7,7 @@ from item import Item, ItemList, MyItems
 from security import authenticate, identify
 app = Flask(__name__)
 api = Api(app)
-app.secret_key = "soroush"
+app.secret_key = os.environ["SECRET_KEY"]
 jwt = JWT(app, authenticate, identify)
 api.add_resource(Item , "/item/<string:name>")
 api.add_resource(ItemList, "/items")
